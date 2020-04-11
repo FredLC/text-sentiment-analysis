fb13fdd95d23e662aeb065dfb20a9d6428293cc1
fredlc <fredlefevre@outlook.com>
2020-04-10 20:11:19 -0400

    Initial commit with UI implementation

---
+++ AntiBully/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AntiBully
+//
+//  Created by Fred Lefevre on 2020-04-10.
+//  Copyright © 2020 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var predictionLabel: UILabel!
+    @IBOutlet weak var sentenceTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func analyzeButtonPressed(_ sender: Any) {
+    }
+    
+}
+
