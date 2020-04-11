57034cee25fec44ba6260913433a5b659f8520d1
fredlc <fredlefevre@outlook.com>
2020-04-11 14:07:29 -0400

    Connected controller to view model

--- AntiBully/Controller/ViewController.swift
+++ AntiBully/Controller/ViewController.swift
@@ -8,17 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var predictionLabel: UILabel!
     @IBOutlet weak var sentenceTextField: UITextField!
     
-
+    var viewModel: ViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        sentenceTextField.delegate = self
+        sentenceTextField.becomeFirstResponder()
     }
 
     @IBAction func analyzeButtonPressed(_ sender: Any) {
+        guard sentenceTextField.text != "" else {
+            predictionLabel.text = "Please enter a sentence below."
+            return
+        }
+        
+        guard let sentence = sentenceTextField.text else { return }
+        viewModel = ViewModel(textData: sentence)
+        predictionLabel.text = viewModel.sentiment.text
+        sentenceTextField.text = ""
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        analyzeButtonPressed(sentenceTextField as Any)
+        return true
     }
     
 }
